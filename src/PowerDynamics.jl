d19060cc6933aba4d89861cbe6169dbf45e555ea
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-06-30 04:45:57 +0000

    Snapshot of branch master taken on 2019-06-30T00:45:56.467-04:00 from "https://github.com/UnofficialJuliaMirror/PowerDynamics.jl-cc46b21f-1b17-51c2-b642-af74c3d3a8be"

--- src/PowerDynamics.jl
+++ src/PowerDynamics.jl
@@ -1,13 +1,61 @@
 # (C) 2018 Potsdam Institute for Climate Impact Research, authors and contributors (see AUTHORS file)
 # Licensed under GNU GPL v3 (see LICENSE file)
 
-__precompile__()
-
 module PowerDynamics
 
-using Reexport
-@reexport using PowerDynBase
-@reexport using PowerDynOperationPoint
-@reexport using PowerDynSolve
+using Markdown # for the @doc
 
-end # DPSA
+include("common/Errors.jl")
+include("common/Helpers.jl")
+include("common/PowerGrid.jl")
+include("common/States.jl")
+
+include("parsers/CSVParser.jl")
+
+# all possible node dynamics
+include("nodes/NodeMacro.jl")
+include("nodes/PQAlgebraic.jl")
+include("nodes/PVAlgebraic.jl")
+include("nodes/SlackAlgebraic.jl")
+include("nodes/SwingEquation.jl")
+include("nodes/FourthOrderEq.jl")
+include("nodes/FourthOrderEqGovernorExciterAVR.jl")
+include("nodes/VoltageSourceInverterMinimal.jl")
+include("nodes/VoltageSourceInverterVoltagePT1.jl")
+include("nodes/CurrentSourceInverterMinimal.jl")
+include("nodes/ExponentialRecoveryLoad.jl")
+
+# all line types
+include("lines/LineMacro.jl")
+include("lines/StaticLine.jl")
+include("lines/PiModelLine.jl")
+
+include("operationpoint/operationpoint.jl")
+
+include("simulations/PowerGridSolutions.jl")
+include("simulations/simulations.jl")
+
+
+# export of the main types and functions
+export GridDynamicsError, NodeDynamicsError, MissingParameterError, StateError
+export no_internal_masses
+export @DynamicNode, showdefinition
+export construct_vertex
+
+export State
+
+export @Line, StaticLine
+export construct_edge
+
+export convert, promote_rule # only so the autodocs work properly
+
+export find_operationpoint
+
+export read_network_from_csv
+export PowerGrid
+export ode_function
+export systemsize
+export symbolsof
+export total_current
+
+end
